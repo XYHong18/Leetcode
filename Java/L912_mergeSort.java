cffe9ff83c76a827082751dc6dbfacde759f2ff3
xh82 <xh82@duke.edu>
2020-05-02 18:02:38 -0400

    912_mergeSort in both Python and Java

--- Java/L912_mergeSort.java
+++ Java/L912_mergeSort.java
@@ -1,11 +1,61 @@
 
 public class L912_mergeSort {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums);
+        mergeSort(nums, 0, nums.length-1);
         return nums;
     }
-
-    public void mergeSort(int[] nums) {
+                  
+    private void merge(int[] A, int l, int p, int r) {
+        int n1 = p-l+1;
+        int n2 = r-p;
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+        
+        for (int i=0; i<n1; i++) {
+            L[i] = A[l+i];
+        }
+        
+        for (int j=0; j<n2; j++) {
+            R[j] = A[p+j+1];
+        }
+        
+        int k = l;
+        int k1 = 0;
+        int k2 = 0;
+        
+        while (k1<n1 && k2<n2) {
+            if (L[k1] < R[k2]) {
+                A[k] = L[k1];
+                k1++;
+            } else {
+                A[k] = R[k2];
+                k2++;
+            }
+            k++;
+        }
+        
+        while (k1<n1) {
+            A[k] = L[k1];
+            k1++;
+            k++;
+        }
+        
+        while (k2<n2) {
+            A[k] = R[k2];
+            k2++;
+            k++;
+        }
+        
+        
+    }
+    
+    private void mergeSort(int[] A, int l, int r) {
+        if (l < r) {
+            int p = (l+r) / 2;
+            mergeSort(A, l, p);
+            mergeSort(A, p+1, r);
+            merge(A, l, p, r);
+        }
         
     }
 
--- Python/912_mergeSort.py
+++ Python/912_mergeSort.py
@@ -3,31 +3,41 @@ def sortArray(nums):
     :type nums: List[int]
     :rtype: List[int]
     """
-    mergeSort(nums)
+    mergeSort(nums, 0, len(nums)-1)
     return nums
 
-def mergeSort(A):
-    if len(A) > 1:
-        mid = len(A) // 2
-        L = A[:mid]
-        R = A[mid:]
-
-        i = j = k = 0
-
-        while i<len(L) and j<len(R):
-            if L[i] < R[j]:
-                A[k] = L[i]
-                i += 1
-            else:
-                A[k] = R[j]
-                j += 1
-            k +=1
+def merge(A, l, p, r):
+    n1 = p-l+1
+    n2 = r-p
         
-        if i < len(L):
-            A[k:] = L[i:]
+    L = A[l:p+1]
+    R = A[p+1:r+1]
+        
+    i = j = 0
+    k = l
+        
+    while i<n1 and j<n2:
+        if L[i] < R[j]:
+            A[k] = L[i]
+            i += 1
+        else:
+            A[k] = R[j]
+            j += 1
+        k += 1
+        
+    if i < n1:
+        A[k:] = L[i:]
+        
+    if j < n2:
+        A[k:] = R[j:]
 
-        if j < len(R):
-            A[k:] = R[j:]
+
+def mergeSort(A, l, r):
+    if l < r:
+        p = (l+r)//2
+        mergeSort(A, l, p)
+        mergeSort(A, p+1, r)
+        merge(A, l, p, r)
 
 
 ########## Complexity Analysis #########
