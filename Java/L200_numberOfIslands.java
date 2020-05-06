ede85d25b5a14cae97d6f0535f51e588ec17293d
xh82 <xh82@duke.edu>
2020-05-06 16:29:08 -0400

    Complete 200

---
+++ Java/L200_numberOfIslands.java
@@ -0,0 +1,51 @@
+
+public class L200_numberOfIslands {
+    public int numIslands(char[][] grid) {
+        int res = 0;
+        int m = grid.length;
+        
+        if (m!=0) {
+            int n = grid[0].length;
+            
+            for (int i=0; i<m; i++) {
+                for (int j=0; j<n; j++) {
+                    if (grid[i][j] == '1') {
+                        res++;
+                        dfs(grid, i, j, m, n);
+                    }
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    private void dfs(char[][] grid, int i, int j, int m, int n) {
+        grid[i][j] = '*';
+        
+        if (i+1<m && grid[i+1][j] == '1') {
+            dfs(grid, i+1, j, m, n);
+        }
+        
+        if (j+1<n && grid[i][j+1] == '1') {
+            dfs(grid, i, j+1, m, n);
+        }
+        
+        if (i-1>=0 && grid[i-1][j] == '1') {
+            dfs(grid, i-1, j, m, n);  
+        }
+        
+        if (j-1>=0 && grid[i][j-1] == '1') {
+            dfs(grid, i, j-1, m, n);
+        }
+        
+    }
+}
+
+
+
+/* 
+Complexity Analysis
+1. Running time: O(mn)
+2. Space complexity: O(mn) ???
+*/
--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # Leetcode Solutions
 
 ## 1. Two Sum
-### Method: Hash Table
+### [x] Method: Hash Table
 
 ## 53. Maximum Subarray
-### Method 1: Divide and conquer:  
+### [x] Method 1: Divide and conquer:  
 Any contiguous subarray A[i...j] of A[low...high] must lie in exactly one of the following places:  
 (1) entirely in the subarray A[low...mid], so that low <= i <= j <= mid;  
 (2) entirely in the subarray A[mid...high], so that mid <= i <= j <= high;  
 (3) or crossing the midpoint, so that low <= i <= mid <= j <= high.
-### Method 2: Dynamic Programming:    
+### [x] Method 2: Dynamic Programming:    
 Define the largest sum of the subarray that ends at nums[i] as dp[i], then  
 dp[i] = max(nums[i], nums[i]+dp[i-1]) 
 
+## 200. Number of Islands
+### [x] Method 1: DFS
+### [] Method 2: BFS
+### [] Method 3: Union Find
+
 ## 912. Sort an Array
-### Method 1: Merge-sort
-### Method 2: Quick-sort  
+### [x] Method 1: Merge-sort
+### [x] Method 2: Quick-sort  
 (The difference between the two methods can be found here: https://www.geeksforgeeks.org/quicksort-better-mergesort/.)
 
 
