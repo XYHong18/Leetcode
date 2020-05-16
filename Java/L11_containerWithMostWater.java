7efa450af6ad6b1b8ac3d5753cd51639d20e0bf9
xh82 <xh82@duke.edu>
2020-05-16 18:31:45 -0400

    Complete 11. Container with most water

---
+++ Java/L11_containerWithMostWater.java
@@ -0,0 +1,26 @@
+public class L11_containerWithMostWater {
+    public int maxArea(int[] height) {
+        int res = 0;
+        int i = 0, j = height.length-1;
+        int area;
+        
+        while (i < j) {
+            area = (j-i) * Math.min(height[i], height[j]);
+            res = Math.max(area, res);
+            
+            if (height[i] < height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
+    }
+}
+
+
+/*
+Complexity Analaysis:
+1. Time Analysis: O(n)
+2. Space Analysis: O(1)
+*/
